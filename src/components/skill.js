735e8529be8c60fc838f59329e311eb98b305d72
ninjasKYU <2066apple@naver.com>
2021-07-22 20:49:15 +0900

    change skill svg image

--- src/components/skill.js
+++ src/components/skill.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 import { AiFillPushpin } from 'react-icons/ai';
-import { FaReact, FaSass, FaHtml5, FaCss3Alt, FaJsSquare, FaGitAlt, FaGithub } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
+import {ReactComponent as Jira} from '../images/jira.svg';
+import {ReactComponent as Zeplin} from '../images/zeplin.svg';
+import {ReactComponent as Typescript} from '../images/typescript.svg';
+import {ReactComponent as HtmlIcon} from '../images/html.svg';
+import {ReactComponent as CssIcon} from '../images/css.svg';
+import {ReactComponent as JavascriptIcon} from '../images/javascript.svg';
+import {ReactComponent as ReactIcon} from '../images/react.svg';
+import {ReactComponent as SassIcon} from '../images/sass.svg';
+import {ReactComponent as GitIcon} from '../images/git.svg';
+import {ReactComponent as PostgresqlIcon} from '../images/postgresql.svg';
+import {ReactComponent as JavaIcon} from '../images/java.svg';
+import {ReactComponent as SpringIcon} from '../images/spring.svg';
 import skillBack from '../images/skillBack.jpg';
 
 const SkillZone = styled.div`
@@ -33,6 +45,10 @@ const SkillZone = styled.div`
 			margin-right: 10px;
 		}
 	}
+	.display-flex {
+		display: flex;
+		justify-content: space-evenly;
+	}
 `;
 
 export const Skill = () => {
@@ -47,19 +63,23 @@ export const Skill = () => {
 					<div>Front-end</div>
 				</div>
 				<div className={"image-contents"}>
-					<FaHtml5/><FaCss3Alt/><FaJsSquare/><FaReact/><FaSass/>
+					<HtmlIcon/><CssIcon/><JavascriptIcon/><Typescript/><ReactIcon/><SassIcon/>
 				</div>
 				<div className={"skill-subTitle"}>
-					<div>Communication</div>
+					<div>Back-end</div>
 				</div>
 				<div className={"image-contents"}>
-					jira zeplin
+					<JavaIcon/><SpringIcon/><PostgresqlIcon/>
 				</div>
-				<div className={"skill-subTitle"}>
-					<div>Version-Tool</div>
-				</div>
-				<div className={"image-contents"}>
-					<FaGitAlt/><FaGithub/>
+				<div>
+					<div className={"skill-subTitle display-flex"}>
+						<div>Communication</div>
+						<div>Version-Tool</div>
+					</div>
+					<div className={"image-contents display-flex"}>
+						<div><Jira/><Zeplin/></div>
+						<div><GitIcon/><FaGithub/></div>
+					</div>
 				</div>
 			</SkillZone>
 		</>
